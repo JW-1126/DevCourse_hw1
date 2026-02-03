3e1140293155e426928512926eef3ff5272b0c37
JW-1126 <jwyoon1126@naver.com>
2026-02-03 18:38:45 +0900

    refactor(controller): annotation기반 로직 호출 컨트롤러 구조 구현

--- src/main/java/controller/CommandRegistry.java
+++ src/main/java/controller/CommandRegistry.java
@@ -5,9 +5,9 @@ import java.util.Optional;
 
 public enum CommandRegistry {
     CREATE("등록"),
-    DELETE("삭제"),
-    UPDATE("수정"),
     READ("목록"),
+    UPDATE("수정"),
+    DELETE("삭제"),
     BUILD("빌드"),
     CLOSE("종료");
 
@@ -17,13 +17,13 @@ public enum CommandRegistry {
         this.inputMessage = inputMessage;
     }
 
-    public boolean inputMatches(String input) {
-        return inputMessage.equals(input);
-    }
-
     public static Optional<CommandRegistry> getCommandRegistry(String input) {
         return Arrays.stream(CommandRegistry.values())
                 .filter(cr -> cr.inputMatches(input))
                 .findFirst();
+    }
+
+    private boolean inputMatches(String input) {
+        return inputMessage.equals(input);
     }
 }
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -3,21 +3,60 @@ package controller;
 import static controller.CommandValidator.validateInput;
 import static view.Output.printError;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import service.CommandManager;
+import service.CommandMapping;
+
 public class Controller {
 
     private final CommandManager commandManager;
+    private final Map<CommandRegistry, Method> commandMap;
 
     public Controller(CommandManager commandManager) {
         this.commandManager = commandManager;
+        this.commandMap = new HashMap<>();
+        init();
     }
 
     public void work(String input) {
         try {
-            // 입력값에 대한 최초 검증 수행 후 전달
             validateInput(input);
-            commandManager.execute(CommandInfo.of(input));
-        } catch (RuntimeException e) {
-            printError(e);
+            // Annotation 기반 전환
+            execute(input);
+        } catch (InvocationTargetException e) {
+            printError((Exception) e.getTargetException());
+        } catch (IllegalAccessException e) {
+            printError((Exception) e.getCause());
         }
+    }
+
+    private void init() {
+        Method[] methods = commandManager.getClass().getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(CommandMapping.class)) {
+                CommandRegistry inputCommand = method.getAnnotation(CommandMapping.class).value();
+                commandMap.put(inputCommand, method);
+            }
+        }
+    }
+
+    private void execute(String input) throws InvocationTargetException, IllegalAccessException {
+        CommandRegistry inputCommand = CommandRegistry
+                .getCommandRegistry(input.split("\\?")[0]).get();
+        Method method = commandMap.get(inputCommand);
+
+        Class<?>[] parameterType = method.getParameterTypes();
+        if (parameterType.length == 1) {
+            int args = Integer.parseInt(
+                    input.split("\\?")[1].split("=")[1]
+            );
+            method.invoke(commandManager, args);
+            return;
+        }
+
+        method.invoke(commandManager);
     }
 }
